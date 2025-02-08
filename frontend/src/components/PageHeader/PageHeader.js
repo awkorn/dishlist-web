dbf5f922277353d385ab5ad655e77695c17b8f73
Korn, Alexander W <alexk9255@gmail.com>
2025-02-07 22:41:50 -0600

    added logo

--- frontend/src/components/PageHeader/PageHeader.js
+++ frontend/src/components/PageHeader/PageHeader.js
@@ -1,10 +1,18 @@
 import React from "react";
+import logo from "../../assets/images/logo-color.png";
+import "./PageHeader.css";
 
 const PageHeader = ({ title }) => {
   return (
-    <header className="page-header">
-      <h1>{title}</h1>
-    </header>
+    <>
+      <header className="logo-banner">
+        <img src={logo} alt="DishList Logo" className="logo" />
+      </header>
+
+      <header className="page-header">
+        <h1>{title}</h1>
+      </header>
+    </>
   );
 };
 
