505ac8409ff8632dacf2beea4c59170ed1afe4a2
Korn, Alexander W <alexk9255@gmail.com>
2025-02-09 15:56:21 -0600

    start dishlist page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignInPage from "./pages/SignInPage";
 import SignUpPage from "./pages/SignUpPage";
 import NotFound from "./components/NotFound/NotFound";
+import DishListsPage from "./pages/DishListsPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<SignInPage />} />
         <Route path="/signin" element={<SignInPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/dishlists" element={<DishListsPage />} />
         <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
     </Router>
---
+++ frontend/src/pages/DishListsPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DishListsPage = () => {
+    return (
+        <>
+        <h1>DishLists</h1>
+        </>
+    )
+}
+
+export default DishListsPage;
