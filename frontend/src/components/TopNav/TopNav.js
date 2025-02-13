3d37dfa925b7c71d83d36df1e6a139cbfe11626e
Korn, Alexander W <alexk9255@gmail.com>
2025-02-12 21:38:22 -0600

    top nav css

--- frontend/src/components/TopNav/TopNav.js
+++ frontend/src/components/TopNav/TopNav.js
@@ -3,11 +3,12 @@ import Logo from "./Logo";
 import SearchBar from "./SearchBar";
 import NavButtons from "./NavButtons";
 import UserMenu from "./UserMenu";
+import "./TopNav.css"
 
 const TopNav = ({pageType}) => {
 
     return(
-        <div>
+        <div className="top-nav">
             <Logo />
             <SearchBar pageType={pageType}/>
             <NavButtons />
