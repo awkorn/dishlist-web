61ee19c9739d4fb5a0de5b15bc1ad40345d8b971
Korn, Alexander W <alexk9255@gmail.com>
2025-02-14 16:28:58 -0600

    start dishlist frontend

---
+++ frontend/src/components/DishListTile/DishListTile.js
@@ -0,0 +1,22 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios"; 
+import "./DishListTile.css";
+
+const DishListTile = ({userId}) => {
+
+    const [dishlists, setDishLists] = useState([]);
+    const [showMenu, setShowMenu] = useState(false);
+    const [selectedDishList, setSelectedDishList] = useState(null);
+    const [showConfirmation, setShowConfirmation] = useState(false);
+
+
+
+    return(
+        <>
+        </>
+    )
+
+
+}
+
+export default DishListTile;
