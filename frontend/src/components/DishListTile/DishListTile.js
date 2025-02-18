297dec7b18a34c0cfc2408ee2cbb7a718148be5d
Korn, Alexander W <alexk9255@gmail.com>
2025-02-18 08:05:28 -0600

    fetch dishlists on frontend

--- frontend/src/components/DishListTile/DishListTile.js
+++ frontend/src/components/DishListTile/DishListTile.js
@@ -1,19 +1,40 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
 import "./DishListTile.css";
+import { useAuth } from "../../contexts/AuthProvider";
 
-const DishListTile = ({userId}) => {
+const FETCH_DISHLISTS = gql`
+  query GetDishLists($userId: String!) {
+    getDishLists(userId: $userId) {
+      id
+      title
+      isPinned
+    }
+  }
+`;
 
-    const [dishlists, setDishLists] = useState([]);
-    const [showMenu, setShowMenu] = useState(false);
-    const [selectedDishList, setSelectedDishList] = useState(null);
-    const [showConfirmation, setShowConfirmation] = useState(false);
+const DishListTile = () => {
+  const { currentUser } = useAuth();
 
-    return(
-        <>
-        </>
-    )
+  const { loading, error, data } = useQuery(FETCH_DISHLISTS, {
+    skip: !currentUser,
+    variables: { userId: currentUser.uid },
+  });
 
+  if (!currentUser) return <p>Please log in to view your DishLists.</p>;
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error fetching Dishlists!</p>;
 
-}
+  return (
+    <div>
+      {data.getDishLists.map((dishlist) => (
+        <div key={dishlist.id}>
+          <h3>{dishlist.title}</h3>
+          {dishlist.isPinned && <span>📌 Pinned</span>}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default DishListTile;
--- frontend/src/pages/DishListsPage.js
+++ frontend/src/pages/DishListsPage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import TopNav from "../components/TopNav/TopNav";
 import PageTitle from "../components/PageTitle/PageTitle";
+import DishListTile from "../components/DishListTile/DishListTile";
 
 const DishListsPage = () => {
   return (
     <>
       <TopNav pageType="dishlists"></TopNav>
       <PageTitle title="DishLists"></PageTitle>
+      <DishListTile></DishListTile>
     </>
   );
 };
