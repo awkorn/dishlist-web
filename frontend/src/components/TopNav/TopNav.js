421f024ddd75c72d3121cbad3d8b1e5b65c3454e
Korn, Alexander W <alexk9255@gmail.com>
2025-02-22 19:26:11 -0600

    css adjustments for small screens

--- frontend/src/components/TopNav/TopNav.js
+++ frontend/src/components/TopNav/TopNav.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "./Logo";
 import SearchBar from "./SearchBar";
 import NavButtons from "./NavButtons";
@@ -6,11 +6,23 @@ import AuthButtons from "./AuthButtons";
 import "./TopNav.css";
 
 const TopNav = ({ pageType, items, onSearch }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen((prev) => !prev);
+  };
+
   return (
-    <div>
-      <div className="top-nav">
-        <Logo />
-        <SearchBar pageType={pageType} items={items} onSearch={onSearch} />
+    <div className="top-nav">
+      <Logo />
+      <SearchBar pageType={pageType} items={items} onSearch={onSearch} />
+
+      {/*hamburger menu icon (visible on small screens via CSS) */}
+      <div className="menu-icon" onClick={toggleMenu}>
+        ☰
+      </div>
+
+      <div className={`nav-container ${menuOpen ? "open" : ""}`}>
         <NavButtons />
         <AuthButtons />
       </div>
