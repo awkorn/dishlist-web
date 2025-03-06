5c12b4ca7be9e50de56948ff55250d9553ff2606
Korn, Alexander W <alexk9255@gmail.com>
2025-03-05 18:01:47 -0600

    dishlist detail queries

---
+++ frontend/src/graphql/queries/dishListDetail.js
@@ -0,0 +1,48 @@
+import { gql } from "@apollo/client";
+
+export const GET_DISHLIST_DETAIL = gql`
+  query GetDishList($id: ID!, $userId: String!) {
+    getDishList(id: $id, userId: $userId) {
+      id
+      userId
+      title
+      description
+      isPinned
+      followers
+      collaborators
+      visibility
+      sharedWith
+      followRequests
+    }
+  }
+`;
+
+export const GET_DISHLIST_RECIPES = gql`
+  query GetDishListRecipes($dishListId: ID!, $userId: String!) {
+    getDishListRecipes(dishListId: $dishListId, userId: $userId) {
+      id
+      creatorId
+      title
+      ingredients {
+        name
+        amount
+        unit
+      }
+      instructions
+      cookTime
+      prepTime
+      servings
+      tags
+      image
+      comments {
+        id
+        userId
+        username
+        content
+        createdAt
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
