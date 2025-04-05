d327a6c57c24571eaf30047419f9ed65e9164fca
Korn, Alexander W <alexk9255@gmail.com>
2025-04-05 12:05:03 -0500

    get rid of logo banner

--- frontend/src/components/common/PageHeader/PageHeader.js
+++ frontend/src/components/common/PageHeader/PageHeader.js
@@ -1,15 +1,10 @@
 import React from "react";
-import logo from "../../../assets/images/logo-color.png";
 import styles from "./PageHeader.module.css";
 
 /* Page header used on sign-in and sign-up page */
 const PageHeader = ({ title }) => {
   return (
     <>
-      <header className={styles.logoBanner}>
-        <img src={logo} alt="DishList Logo" className={styles.logo} />
-      </header>
-
       <header className={styles.pageHeader}>
         <h1>{title}</h1>
       </header>
