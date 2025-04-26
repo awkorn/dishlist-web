78e79127fe4d41e744fb9e7e7bf67bd127adf9c2
Korn, Alexander W <alexk9255@gmail.com>
2025-04-25 20:52:15 -0500

    auth pages add banner

--- frontend/src/features/auth/pages/SignInPage.js
+++ frontend/src/features/auth/pages/SignInPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import PageHeader from "../../../components/common/PageHeader/PageHeader"
 import SignInForm from "../components/SignInForm/SignInForm"
+import AuthBanner from "../components/AuthBanner/AuthBanner";
 
 const SignInPage = () => {
   return (
     <div className="sign-in-page">
+      <AuthBanner />
       <PageHeader title="Welcome Back!" />
       <SignInForm />
     </div>
--- frontend/src/features/auth/pages/SignUpPage.js
+++ frontend/src/features/auth/pages/SignUpPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import PageHeader from "../../../components/common/PageHeader/PageHeader"
 import SignUpForm from "../components/SignUpForm/SignUpForm";
+import AuthBanner from "../components/AuthBanner/AuthBanner";
 
 const SignUpPage = () => {
   return (
     <div>
+      <AuthBanner />
       <PageHeader title="Create Account" />
       <SignUpForm />
     </div>
