446e4b3542579a156c6bcc2141ad893661129e0e
Korn, Alexander W <alexk9255@gmail.com>
2025-05-16 17:25:12 -0500

    frontend deploy

--- frontend/src/services/apolloClient.js
+++ frontend/src/services/apolloClient.js
@@ -1,7 +1,13 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
+const isProd = process.env.NODE_ENV === 'production';
+const backendUrl = process.env.REACT_APP_BACKEND_API_URL || 
+  (isProd ? 'https://dishlist-web.vercel.app' : 'http://localhost:5000');
+
 const client = new ApolloClient({
-  link: new HttpLink({ uri: "http://localhost:5000/graphql" }),
+  link: new HttpLink({ 
+    uri: `${backendUrl}/graphql` 
+  }),
   cache: new InMemoryCache(),
 });
 
