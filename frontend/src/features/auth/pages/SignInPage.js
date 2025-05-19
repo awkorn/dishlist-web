d9947c7815c4884758ec5fc94f844dc3c24ff16d
Korn, Alexander W <alexk9255@gmail.com>
2025-05-18 20:06:37 -0500

    auth pages css mobile updates

--- frontend/src/features/auth/pages/SignInPage.js
+++ frontend/src/features/auth/pages/SignInPage.js
@@ -1,14 +1,19 @@
 import React from "react";
-import PageHeader from "../../../components/common/PageHeader/PageHeader"
-import SignInForm from "../components/SignInForm/SignInForm"
+import PageHeader from "../../../components/common/PageHeader/PageHeader";
+import SignInForm from "../components/SignInForm/SignInForm";
 import AuthBanner from "../components/AuthBanner/AuthBanner";
+import styles from "./AuthPages.module.css";
 
 const SignInPage = () => {
   return (
-    <div className="sign-in-page">
+    <div className={styles.authPageContainer}>
       <AuthBanner />
-      <PageHeader title="Welcome Back!" />
-      <SignInForm />
+      <div className={styles.authContent}>
+        <PageHeader title="Welcome Back!" />
+        <div className={styles.formContainer}>
+          <SignInForm />
+        </div>
+      </div>
     </div>
   );
 };
--- frontend/src/features/auth/pages/SignUpPage.js
+++ frontend/src/features/auth/pages/SignUpPage.js
@@ -1,16 +1,21 @@
 import React from "react";
-import PageHeader from "../../../components/common/PageHeader/PageHeader"
+import PageHeader from "../../../components/common/PageHeader/PageHeader";
 import SignUpForm from "../components/SignUpForm/SignUpForm";
 import AuthBanner from "../components/AuthBanner/AuthBanner";
+import styles from "./AuthPages.module.css";
 
 const SignUpPage = () => {
   return (
-    <div>
+    <div className={styles.authPageContainer}>
       <AuthBanner />
-      <PageHeader title="Create Account" />
-      <SignUpForm />
+      <div className={styles.authContent}>
+        <PageHeader title="Create Account" />
+        <div className={styles.formContainer}>
+          <SignUpForm />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default SignUpPage;
